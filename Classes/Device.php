27977dbffe225da53f503e1b39512b4104657e56
S-Ausleihe <s-ausleihe@esfl.de>
2020-02-13 11:02:58 +0100

    Created a Class for the database table 'device'. Implemented necessary methods.

--- Classes/Device.php
+++ Classes/Device.php
@@ -8,42 +8,56 @@
  */
 class Device {
     public $deviceID; // numer of the device
-    public $type;   // type of the device
     public $name; // name of the device
+    public $type;   // type of the device
     public $brand;  // brand of the device
     public $qrCode; // QR-Code of the device
     public $cardId; //number of the cart
-    public $link;   //link to show details
+	
+	public function __construct() {
+		$this->DB = Database::getDbConnection()
+	}
     
-    /***
-     * Create a new object from device.
-     */
-    public function __construct() {
-        $this->deviceID = $_POST['C1'];
-        $this->type = $_POST['C2'];
-        $this->name = $_POST['C3'];
-        $this->brand = $_POST['C4'];
-        $this->qrCode = $_POST['C5']; 
-    }
-    
-    /***
-     * Add new devices.
-     */
-    public function addDevice($type, $name, $brand, $qrCode, $link){
-        
-    }
-    
-    /***
-     * Delete existed devices.
-     */
-    public function deleteDevice($deviceId){
-        
-    }
-    
-    /***
-     * Change the type, name, brand or qrCode of the devices.
-     */
-    public function modifyDevice($type, $name, $brand, $qrCode, $link) {
-        
-    }
+    /**
+     * it saves the record
+     **/
+    abstract public function save() {
+
+	}
+	
+    /**
+     * it deletes the record
+     **/
+    abstract public function delete($deviceID) {
+		$query = "DELETE FROM device WHERE ´deviceID´ = ?";
+		$param = ($deviceID);
+		$DB->executeQuery($query, $param)
+	}
+	
+    /**
+     * it inserts the record
+     **/
+    abstract protected function insert($name, $type, $brand) {
+		$query = "INSERT INTO device (name, type, brand) VALUES (?, ?, ?)";
+		$param = ($name, $type, $brand);
+		$DB->executeQuery($query, $param)
+	}
+	
+    /**
+     * it updates the record
+     **/
+    abstract protected function update($deviceID, $name, $type, $brand) {
+		$query = "UPDATE device SET (name = ?, type = ?, brand = ?) WHERE ´deviceID´ = ?";
+		$param = ($name, $type, $brand, $deviceID);
+		$DB->executeQuery($query, $param)
+	}
+	
+     /**
+     * @return a specific ID
+     **/
+    abstract static function load($deviceID):ActiveRecord {
+		$query = "SELECT * FROM device WHERE ´deviceID´ = ?";
+		$param = ($deviceID);
+		$DB->executeQuery($query, $param)
+	}
 }
